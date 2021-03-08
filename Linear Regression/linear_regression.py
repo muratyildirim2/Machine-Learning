1740a498b5e8f61b16d0b4d11c1797d2a70ca16e
muratyildirim2 <78560039+muratyildirim2@users.noreply.github.com>
2021-03-08 14:37:29 +0200

    Add files via upload

---
+++ Linear Regression/linear_regression.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+
+df = pd.read_csv("linear_regression_dataset.csv",sep=";")
+
+
+
+
+
+
+linear_reg=LinearRegression()
+
+x=df.deneyim.values.reshape(-1,1)
+y=df.maas.values.reshape(-1,1)
+
+
+linear_reg.fit(x,y)
+
+b0 = linear_reg.predict([[0]])
+
+array = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]).reshape(-1,1)
+
+
+plt.scatter(x,y)
+
+y_head = linear_reg.predict(array)
+
+plt.plot(array,y_head,color="red")
