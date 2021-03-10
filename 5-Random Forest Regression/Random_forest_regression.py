d0efa221b41b1dedd4901eec79db151725bcb55b
muratyildirim2 <78560039+muratyildirim2@users.noreply.github.com>
2021-03-10 10:46:41 +0200

    Add files via upload

---
+++ 5-Random Forest Regression/Random_forest_regression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 10:01:18 2021
+
+@author: MURAT
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+
+df = pd.read_csv("random forest regression dataset.csv",sep=";",header=None)
+
+x = df.iloc[:,0].values.reshape(-1,1)
+
+y = df.iloc[:,1].values.reshape(-1,1)
+
+
+rf = RandomForestRegressor(n_estimators=100,random_state=(42))
+
+rf.fit(x,y)
+
+
+x_=  np.arange(min(x),max(x),0.01).reshape(-1,1)
+
+y_head=rf.predict(x_)
+
+plt.scatter(x, y, color="red")
+plt.plot(x_,y_head,color="green")
+
+plt.xlabel("Tribün Sınıfı")
+plt.ylabel("Ücret")
+
+
+plt.show()
