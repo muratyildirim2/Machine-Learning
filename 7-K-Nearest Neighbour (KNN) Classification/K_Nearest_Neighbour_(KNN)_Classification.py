300fe3f1f1419ca73114ee374c5db7b7fda151b4
muratyildirim2 <78560039+muratyildirim2@users.noreply.github.com>
2021-03-23 16:41:34 +0200

    Add files via upload

---
+++ 7-K-Nearest Neighbour (KNN) Classification/K_Nearest_Neighbour_(KNN)_Classification.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+# Author : Murat Yıldırım
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# In[34]:
+
+
+data = pd.read_csv("data.csv")
+
+
+# In[35]:
+
+
+data.drop(["id","Unnamed: 32"],axis=1,inplace=True)
+data.head()
+
+
+# In[36]:
+
+
+M = data[data.diagnosis == "M"]
+B = data[data.diagnosis == "B"]
+
+
+# In[37]:
+
+
+plt.scatter(M.radius_mean,M.texture_mean,color ="red",label="kotu",alpha=0.2)
+plt.scatter(B.radius_mean,B.texture_mean,color ="green",label="iyi",alpha=0.2)
+plt.xlabel("radius_mean")
+plt.ylabel("texture_mean")
+plt.legend()
+plt.show()
+
+
+# In[38]:
+
+
+data.diagnosis=[1 if each == "M" else 0 for each in data.diagnosis ]
+y=data.diagnosis.values
+x_data=data.drop(["diagnosis"],axis=1)
+
+
+# In[39]:
+
+
+x= (x_data- np.min(x_data))/(np.max(x_data)-np.min(x_data))
+
+
+# In[40]:
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state =1)
+
+
+# In[52]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train,y_train)
+prediction=knn.predict(x_test)
+
+
+# In[53]:
+
+
+print("{} score : {}" .format(3,knn.score(x_test,y_test)))
+
+
+# In[56]:
+
+
+score_list=[]
+for each in range (1,15) :
+    knn2 = KNeighborsClassifier(n_neighbors=each)
+    knn2.fit(x_train,y_train)
+    score_list.append(knn2.score(x_test,y_test))
+
+plt.plot(range(1,15),score_list)
+plt.xlabel("K values")
+plt.ylabel("Accuracy")
+plt.show()
+    
+
+
+# In[ ]:
+
+
+
+
